2faa6cd15e1d68f9d98904ac0274929d4e94e393
Li Zhe <software@bjtu.edu.cn>
2018-03-08 19:49:11 +0800

    add test event

---
+++ event-test.php
@@ -0,0 +1,17 @@
+<?php
+$eventid=$_POST['eventid'];
+$eventname="";
+$response="";
+if ($eventid == "testevent1") {
+	$eventname = "TEST EVENT 1";
+	$response="200";
+} else if ($eventid == "testvent2") {
+	$eventname = "TEST EVENT 2";
+	$response="200";
+} else {
+	$response="404";
+}
+$output = array ('event' => $eventname, 'state' => $response);
+echo json_encode($output);
+	
+?>
